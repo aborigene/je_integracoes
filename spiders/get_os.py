9e654cd16a38a847e74d564c324d421ec7322fc4
Igor Simões <krishnarupa@gmail.com>
2022-03-02 09:12:50 -0300

    handling cookies

--- spiders/get_os.py
+++ spiders/get_os.py
@@ -1,31 +1,77 @@
 # ESTE É UM TRABALHO EM PROGRESSO. O SCRIPT AINDA NÃO COLETA OU CRIA OS, POR EQUANTO APENAS FAZ O POST DO LOGIN E CARREGA A PRINCIPAL
 
+from asyncio.windows_events import NULL
+from http import cookiejar
 import scrapy
 from scrapy.http import FormRequest
 import json
+import re
 
 class ExampleSpider(scrapy.Spider):
-    name = 'PostSpider'
+    name = 'GetOS'
     allowed_domains = ['zencheck.com.br']
+    cookie_jar = NULL
+    cookie = NULL
 
     def start_requests(self):
+        yield scrapy.Request("https://zencheck.com.br/Extintor/Login",
+                                meta={'cookiejar': self.cookie_jar}, 
+                                callback=self.handleNetSessionID,
+                                method='GET')
+        #yield FormRequest('https://zencheck.com.br/Extintor/Login/Login', meta={'cookiejar': self.cookie_jar}, callback=self.redirectPrincipal,
+        #                         method='POST', formdata=params)
+        
+    def handleNetSessionID(self, response):
+        print("Handling .NetSessionID")
+        self.cookie = response.headers.getlist('Set-Cookie')
+        rawAspnetSessionId = str(self.cookie[0])
+        print(rawAspnetSessionId)
+        aspnetSessionId = re.findall("^b'ASP.NET_SessionId=(.*); path", rawAspnetSessionId)
+        print(aspnetSessionId)
+
+        print("Handling Zencheck Login")
+
         params = {
             "Usuario":"igorsimoes",   
             "Senha":"je123!"
             }
-        yield FormRequest('https://zencheck.com.br/Extintor/Login/Login', callback=self.redirectPrincipal,
-                                 method='POST', formdata=params)
+        yield FormRequest('https://zencheck.com.br/Extintor/Login/Login', 
+                            meta={'cookiejar': self.cookie_jar}, 
+                            callback=self.redirectPrincipal,
+                            cookies={"ASP.NET_SessionId": aspnetSessionId[0]},
+                            method='POST', 
+                            formdata=params)
+
+    #def zencheckLogin(self):
         
 
     def redirectPrincipal(self, response):
+        print("Handling Redirect to Principal")
+        self.cookie = response.headers.getlist('Set-Cookie')
         parsed_response = json.loads(response.text)
         print("Resposta Login: "+response.text)
         print("Redirect URL:"+parsed_response["url"])
-        yield scrapy.Request("https://zencheck.com.br"+parsed_response["url"], callback=self.parsePrincipal,
-                                 method='GET')
+        yield scrapy.Request("https://zencheck.com.br"+parsed_response["url"],
+                                meta={'cookiejar': self.cookie_jar}, 
+                                cookies=self.cookie,
+                                callback=self.listaOS,
+                                method='GET')
         pass
 
-    def parsePrincipal(self, response):
+    def parseHTML(self, response):
+        self.cookie = response.headers.getlist('Set-Cookie')
         #parsed_response = json.loads(response.text)
+        print("Resposta HTML: "+response.text)
+        pass
+
+    def listaOS(self, response):
         print("Resposta Principal: "+response.text)
+        self.cookie = response.headers.getlist('Set-Cookie')
+        #parsed_response = json.loads(response.text)
+        
+        yield scrapy.Request("https://zencheck.com.br/Extintor/Oficina/OrdemDeServicoDoExtintor", 
+                                meta={'cookiejar': self.cookie_jar}, 
+                                cookies=self.cookie,
+                                callback=self.parseHTML,
+                                method='GET')
         pass
